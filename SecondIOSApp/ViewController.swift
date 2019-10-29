cf81445e20dcdf5146e4662569e8769dcffd738d
Joe <lyonsj@chc.edu>
2019-10-29 19:07:54 -0400

    Initial Commit

---
+++ SecondIOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondIOSApp
+//
+//  Created by Stacey Burke on 10/29/19.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
